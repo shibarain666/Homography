b8073bfb9dfeb467fc7f29718a2dd5199959023f
Jerry Cheng <jerry293285@gmail.com>
2021-06-03 23:28:01 +0800

    fix compile warnings & modify comments from chinese to english

--- Homography/homography_src.cpp
+++ Homography/homography_src.cpp
@@ -22,12 +22,14 @@ Mat find_H_matrix(vector<Point2f> src, vector<Point2f> dst) {
 		double row1[8] = { src[i].x, src[i].y, 1,        0,        0, 0, -dst[i].x * src[i].x, -dst[i].x * src[i].y };
 		double row2[8] = { 0       ,        0, 0, src[i].x, src[i].y, 1, -dst[i].y * src[i].x, -dst[i].y * src[i].y };
 
-		for (int j = 0; j < 8; j++) { //將4組點資料存成8*8矩陣
+		for (int j = 0; j < 8; j++) { /* save 4 point data to 8*8 Matrix */
 			Point_matrix[2 * i][j] = row1[j];
 			Point_matrix[2 * i + 1][j] = row2[j];
 		}
 	}
-	Mat Point_data(8, 8, CV_64FC1, Point_matrix); //到此以上為做出8*8矩陣
+
+	/* 8*8 Matrix */
+	Mat Point_data(8, 8, CV_64FC1, Point_matrix); 
 
 	double _dst[8];
 	for (int i = 0; i < 4; i++) {
@@ -38,31 +40,32 @@ Mat find_H_matrix(vector<Point2f> src, vector<Point2f> dst) {
 
 	Mat h8 = Point_data.inv() * Point_target;
 
-	double H8[9]; //3*3 H矩陣
+	double H8[9]; /* 3*3 H Matrix */
 	for (int i = 0; i < 8; i++) {
 		H8[i] = h8.at<double>(i, 0);
 	}
-	H8[8] = 1;     //右下角為1
+	H8[8] = 1;
 	Mat H(3, 3, CV_64FC1, H8);
 	return H.clone();
 }
 
 Mat do_transform(Mat src, Mat H) {
-	Mat img(768, 1024, CV_8UC3, Scalar(0, 0, 0)); //宣告一張新圖，用回推到原圖的方式避免掉空洞問題
 
-	for (int i = 0; i < img.size().height; i++) { //跑高
-		for (int j = 0; j < img.size().width; j++) { //跑寬
-			double x_pixel[3] = { j, i, 1 };
-			Mat Xresult(3, 1, CV_64FC1, x_pixel);
+	Mat tgt(768, 1024, CV_8UC3, Scalar(0, 0, 0));
+
+	/* Target-to-Source */
+	for (int i = 0; i < tgt.size().height; i++) {
+		for (int j = 0; j < tgt.size().width; j++) {
+			Mat Xresult = (Mat_<double>(3, 1) << j, i, 1.0);
 			Mat X = H.inv()*Xresult;
-			double x = cvRound(X.at<double>(0, 0) / X.at<double>(2, 0));	//正規化
-			double y = cvRound(X.at<double>(1, 0) / X.at<double>(2, 0));
+			int x = cvRound(X.at<double>(0, 0) / X.at<double>(2, 0));    /* normalized */
+			int y = cvRound(X.at<double>(1, 0) / X.at<double>(2, 0));
 			if (x < src.size().width && y < src.size().height && x >= 0 && y >= 0) {
-				img.at<Vec3b>(i, j) = src.at<Vec3b>(y, x);
+				tgt.at<Vec3b>(i, j) = src.at<Vec3b>(y, x);
 			}
 		}
 	}
-	return img.clone();
+	return tgt.clone();
 }
 
 int main() {
@@ -72,8 +75,8 @@ int main() {
 	Mat img_homo(img_gt.rows, img_gt.cols, CV_8UC3, Scalar(0, 0, 0));
 
 	/* Correspondence Points */
-	vector<Point2f> img_ori_points = { Point2f(559, 529), Point2f(2041, 349), Point2f(573, 1733), Point2f(2053, 1887) }; //直接抓取原圖角落四個點
-	vector<Point2f> img_homo_points = { Point2f(0, 0), Point2f(1024, 0), Point2f(0, 768), Point2f(1024, 768) };          //確保不共線
+	vector<Point2f> img_ori_points = { Point2f(559, 529), Point2f(2041, 349), Point2f(573, 1733), Point2f(2053, 1887) }; /* 4 sets of points on source image */
+	vector<Point2f> img_homo_points = { Point2f(0, 0), Point2f(1024, 0), Point2f(0, 768), Point2f(1024, 768) };          /* 4 sets of points on target image */
 
 	/* OpenCV's API, for comparison use */
 	Mat H_opencv = findHomography(img_ori_points, img_homo_points);
@@ -84,6 +87,8 @@ int main() {
 	cout << H << endl;
 	cout << "OpenCV H matrix:" << endl;
 	cout << H_opencv << endl;
+	cout << "Self-implemented inverse H matrix:" << endl;
+	cout << H.inv() << endl;
 
 	img_homo = do_transform(img_ori, H);
 
